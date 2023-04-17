c12be103500fed0227bf5990fd90507022ed5d12
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-17 18:09:09 -0300

    Breve introdução à inserção de conteúdo HTML

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-// Para este exercicio aplique a lógica necessário para 
-//mostrar os anos que são realmente bissextos, 
-//respeitando a regra: Os anos bissextos são múltiplos de 4, 
-//não múltiplos de 100 (1900 não é bissexto) e múltiplos de 400
-// (2000 é bissexto).
+//criei uma variavel e referenciei ele no html com o ID output
 
-for(var a=1004; a<=2017; a++){
- if(a%4 === 0 || a%400 === 0 ){
-    document.write(a + "<br>");
- }else{
-    if(a%100 === 0){
-    document.write("não é bisexto")
-    }
- }
-}
+var output = document.querySelector('#output');
+
+//inserindo algo nesse elemento html pelo javascript 
+output.innerHTML='Texto inserido por javascript'
