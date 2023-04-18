87c11ce48abe17d8ea0f5a00ee186a89a06d1aca
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-18 18:07:11 -0300

    usando array para inserir texto no html através do js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //criei uma variavel e referenciei ele no html com o ID output
-
 var output = document.querySelector('#output');
 
 //inserindo algo nesse elemento html pelo javascript 
 output.innerHTML='Texto inserido por javascript'
+
+var menssagem = "";
+for (var i = 0; i < 11; i++) {
+    menssagem+="i: "+ i + "<br>";
+}
+
+output.innerHTML = menssagem;
