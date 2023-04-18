8397aaf2add0c5665c4553590c57993ede4b43df
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-18 18:26:30 -0300

    Criando uma função

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+
+//Criar a Função
+function mostrarTabuada(){
+    alert("mostrar Tabuada Chamada");
+}
+//OBRIGATORIO INVOCAR A FUNÇÃO
+mostrarTabuada();
+
 //criei uma variavel e referenciei ele no html com o ID output
 var output = document.querySelector('#output');
-
 //inserindo algo nesse elemento html pelo javascript 
 output.innerHTML='Texto inserido por javascript'
-
+//criando sring vazia
 var menssagem = "";
-for (var i = 0; i < 11; i++) {
-    menssagem+="i: "+ i + "<br>";
-}
 
 output.innerHTML = menssagem;
