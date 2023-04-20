f390ae63d3736c791fdbe5cecafff610518d0ce8
Jonathas Veiga <45939015+jonathasVeiga@users.noreply.github.com>
2023-04-20 20:18:21 -0300

    Adicionando arquivos
    percebi que os arquivos não tinham sido upados

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function mostrarTabuada(){
+    //perguntar o numero para o usuário
+
+    //converter o numero 
+
+    //criar uma variavel que serviré de indice
+
+    /*enquanto <= 1000
+      concatena na variavel menssagem*/
+
+    //mostra menssagem no output 
+}
+
+mostrarTabuada();
+
+var output = document.querySelector('#output');
+output.innerHTML='Texto inserido por javascript'
+
+var menssagem = "";
