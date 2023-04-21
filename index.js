c803d4197b7dea561a8abad5f986a48a382529fb
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-21 14:00:24 -0300

    teste de commit, exercicio criando função

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 function mostrarTabuada(){
     //perguntar o numero para o usuário
-
     //converter o numero 
+    var numero = parseInt("Digite um numero: ");
 
     //criar uma variavel que serviré de indice
-
     /*enquanto <= 1000
       concatena na variavel menssagem*/
+    for (var i =0; i<=100;i++) {
+        var menssagem = i + "x"
+    }
 
     //mostra menssagem no output 
 }
@@ -16,6 +18,4 @@ function mostrarTabuada(){
 mostrarTabuada();
 
 var output = document.querySelector('#output');
-output.innerHTML='Texto inserido por javascript'
-
 var menssagem = "";
