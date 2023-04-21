dda7f671781860100658c515cc9b3106413600a9
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-21 14:48:01 -0300

    resolução do exercicio

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
+var output = document.querySelector("#output");
+var menssagem ="";
 
 function mostrarTabuada(){
     //perguntar o numero para o usuário
     //converter o numero 
-    var numero = parseInt("Digite um numero: ");
+    var numero = parseInt(prompt("Digite um numero: "));
 
     //criar uma variavel que serviré de indice
     /*enquanto <= 1000
       concatena na variavel menssagem*/
-    for (var i =0; i<=100;i++) {
-        var menssagem = i + "x"
+    for (var i = 0; i<=100; i++) {
+        //var menssagem = messagem + numero + "x" + i + "=" + numero *i;
+        menssagem += numero +"x"+ i + "=" + (numero*i)+ "<br>"; 
     }
-
-    //mostra menssagem no output 
+    
+    //mostra menssagem no output
+    output.innerHTML = menssagem; 
 }
-
-mostrarTabuada();
-
-var output = document.querySelector('#output');
-var menssagem = "";
