5aeffd1843fa047c47e2a210d337ae4a7cea8951
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-27 20:53:16 -0300

    explicando o codigo que mostra a tabuada

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ var output = document.querySelector("#output");
 var menssagem ="";
 
 function mostrarTabuada(){
-    //perguntar o numero para o usuário
-    //converter o numero 
     var numero = parseInt(prompt("Digite um numero: "));
-
-    //criar uma variavel que serviré de indice
-    /*enquanto <= 1000
-      concatena na variavel menssagem*/
     for (var i = 0; i<=100; i++) {
-        //var menssagem = messagem + numero + "x" + i + "=" + numero *i;
-        menssagem += numero +"x"+ i + "=" + (numero*i)+ "<br>"; 
+      //menssagem += numero + "x" + i + "=" + numero *i +"<BR>";
+      menssagem            =          menssagem +                                                 (numero +"x"+ i + "=" + (numero*i)+ "<br>"); 
+    //aqui ele cria a variavel      //aqui é onde vai ficar o resultado do comando anterior     //aqui é onde ele vai execultar o comando de tabuada.
+    
     }
     
     //mostra menssagem no output
