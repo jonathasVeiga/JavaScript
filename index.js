03949c332fbd7b7c65814cd178bb6c011fe5319e
JonathasVeiga <jonathas.webd@gmail.com>
2023-04-29 00:07:49 -0300

    criando o layout da calculadora

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var output = document.querySelector("#output");
-var menssagem ="";
+// var output = document.querySelector("#output");
+// var menssagem ="";
 
-function mostrarTabuada(){
-    var numero = parseInt(prompt("Digite um numero: "));
-    for (var i = 0; i<=100; i++) {
-      //menssagem += numero + "x" + i + "=" + numero *i +"<BR>";
-      menssagem            =          menssagem +                                                 (numero +"x"+ i + "=" + (numero*i)+ "<br>"); 
-    //aqui ele cria a variavel      //aqui é onde vai ficar o resultado do comando anterior     //aqui é onde ele vai execultar o comando de tabuada.
+// function mostrarTabuada(){
+//     var numero = parseInt(prompt("Digite um numero: "));
+//     for (var i = 0; i<=100; i++) {
+//       //menssagem += numero + "x" + i + "=" + numero *i +"<BR>";
+//       menssagem            =          menssagem +                                                 (numero +"x"+ i + "=" + (numero*i)+ "<br>"); 
+//     //aqui ele cria a variavel      //aqui é onde vai ficar o resultado do comando anterior     //aqui é onde ele vai execultar o comando de tabuada.
     
-    }
+//     }
     
-    //mostra menssagem no output
-    output.innerHTML = menssagem; 
-}
+//     //mostra menssagem no output
+//     output.innerHTML = menssagem; 
+// }
+
+//mini calculadora 1.0
+
+//var n1 = parseFloat(prompt("digite o primeiro numero para calcular: "));
+
