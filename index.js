a6e8001ff6853a3a1292c338e7c66835cc473a6b
JonathasVeiga <jonathas.webd@gmail.com>
2023-05-02 20:58:08 -0300

    Criando as funções de cada operção matemática

--- index.js
+++ index.js
@@ -7,9 +7,9 @@
 //       //menssagem += numero + "x" + i + "=" + numero *i +"<BR>";
 //       menssagem            =          menssagem +                                                 (numero +"x"+ i + "=" + (numero*i)+ "<br>"); 
 //     //aqui ele cria a variavel      //aqui é onde vai ficar o resultado do comando anterior     //aqui é onde ele vai execultar o comando de tabuada.
-    
+
 //     }
-    
+
 //     //mostra menssagem no output
 //     output.innerHTML = menssagem; 
 // }
@@ -18,18 +18,41 @@
 
 var tela = document.querySelector("#tela");
 var numero = parseInt(prompt("Digite o numero a ser calculado: "));
+var resultado, numero2, numero3 = "";
 
-function mostrarValor(){
+
+function mostrarValor() {
     tela.innerHTML = numero;
 }
 
 mostrarValor();//obrigatório invocar a função.EM ALGUM MOMENTO ELA DEVE SER CHAMADA 
 
-
-function soma(){
+function somar() {
     var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
-    var somar = numero +" + "+ numero2 +" = "+ (numero+numero2);
-    tela.innerHTML = somar;
+    var resultado = numero + " + " + numero2 + " = " + (numero + numero2);
+    tela.innerHTML = resultado;
 }
-    
+
+function subtrair() {
+    var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
+    var resultado = numero + " - " + numero2 + " = " + (numero - numero2);
+    tela.innerHTML = resultado;
+}
+
+function multiplicar() {
+    var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
+    var resultado = numero + " X " + numero2 + " = " + (numero * numero2);
+    tela.innerHTML = resultado;
+}
+
+function dividir() {
+    var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
+    var resultado = numero + " / " + numero2 + " = " + (numero / numero2);
+    tela.innerHTML = resultado;
+}
+
+
+
+
+
 
