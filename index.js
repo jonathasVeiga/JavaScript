2c43d0470c9d2c4c59fb2c3b2e0d64fc7024ca03
JonathasVeiga <jonathas.webd@gmail.com>
2023-05-08 19:54:21 -0300

    resolução do exercicio da calculadora 1.0

--- index.js
+++ index.js
@@ -1,57 +1,39 @@
-// var output = document.querySelector("#output");
-// var menssagem ="";
-
-// function mostrarTabuada(){
-//     var numero = parseInt(prompt("Digite um numero: "));
-//     for (var i = 0; i<=100; i++) {
-//       //menssagem += numero + "x" + i + "=" + numero *i +"<BR>";
-//       menssagem            =          menssagem +                                                 (numero +"x"+ i + "=" + (numero*i)+ "<br>"); 
-//     //aqui ele cria a variavel      //aqui é onde vai ficar o resultado do comando anterior     //aqui é onde ele vai execultar o comando de tabuada.
-
-//     }
-
-//     //mostra menssagem no output
-//     output.innerHTML = menssagem; 
-// }
-
 //mini calculadora 1.0
 
 var tela = document.querySelector("#tela");
 var numero = parseInt(prompt("Digite o numero a ser calculado: "));
-var resultado, numero2, numero3 = "";
-
 
 function mostrarValor() {
     tela.innerHTML = numero;
 }
 
+//aqui ele exibe o primeiro numero 
 mostrarValor();//obrigatório invocar a função.EM ALGUM MOMENTO ELA DEVE SER CHAMADA 
 
 function somar() {
-    var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
-    var resultado = numero + " + " + numero2 + " = " + (numero + numero2);
-    tela.innerHTML = resultado;
+    var numero2 = parseFloat(prompt("Digite segundo numero a ser calculado: "));
+    numero = numero + numero2; //aqui eu altero o valor da VARIAVEL GLOBAL numero
+    //tela.innerHTML = resultado; ao invés de chamar isso eu apenas chamo a função mostrarValor.
+    mostrarValor();//aqui ele chama a função mostrar com o resultado 
 }
 
 function subtrair() {
-    var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
-    var resultado = numero + " - " + numero2 + " = " + (numero - numero2);
-    tela.innerHTML = resultado;
+    var numero2 = parseFloat(prompt("Digite segundo numero a ser calculado: "));
+    numero = numero - numero2;
+    mostrarValor();
 }
 
 function multiplicar() {
     var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
-    var resultado = numero + " X " + numero2 + " = " + (numero * numero2);
-    tela.innerHTML = resultado;
+    numero = numero * numero2;
+    mostrarValor();
 }
 
 function dividir() {
     var numero2 = parseInt(prompt("Digite segundo numero a ser calculado: "));
-    var resultado = numero + " / " + numero2 + " = " + (numero / numero2);
-    tela.innerHTML = resultado;
+    var numero = numero / numero2;
+    mostrarValor();
 }
-
-
 
 
 
